e4bcdf5e389888e41293f5b8ee840c4e8e346867
Noaman-Saleem <noaman.saleem@umt.edu.pk>
2023-11-03 13:38:21 +0500

    JS Files

---
+++ 03-javascript/02-variables/index.js
@@ -0,0 +1,23 @@
+const userId = 12345;
+let userEmail = "nomi@gmail.com";
+var userPassword = "password";
+city = "Lahore";
+
+let userRole;
+
+// {
+// scope in js
+// }
+
+// userId = 1111;
+// userEmail = "Umair";
+
+// console.log(userId);
+// console.log(userEmail);
+// console.log(userPassword);
+// console.log(city);
+
+console.table([userId, userEmail, userPassword, city, userRole]);
+
+// Node. js console is a global object and is used to print different levels of messages to stdout
+console.log(console.log);
